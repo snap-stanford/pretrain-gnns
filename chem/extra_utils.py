0ca76459e0c1504760d8131778fe424f94daf579
LanceKnight <lanceknight26@gmail.com>
2021-06-10 09:46:02 -0500

    extra_utils.py

---
+++ chem/extra_utils.py
@@ -0,0 +1,18 @@
+
+
+def trans_cis_augment(smi):
+	if '=' not in smi:
+		raise f'The molecule does not have double bond! smiles: {smi}'
+	if ''
+
+
+
+
+def augment(smi):
+
+if __name__ == '__main__':
+	print(testing...)
+	smi1 = 'CC'
+	smi2 = 'C=C'
+	smi3 = 'C\C=CCF'
+	smi4 = 'C\C=C/CF'
